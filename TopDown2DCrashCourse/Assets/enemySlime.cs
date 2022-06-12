97993728e4c6b6e60a2b328b42e2073744691df2
Shaina Caballero <scba055@aucklanduni.ac.nz>
2022-06-12 23:25:09 +1200

    Animation of slime taking damage done

--- TopDown2DCrashCourse/Assets/enemySlime.cs
+++ TopDown2DCrashCourse/Assets/enemySlime.cs
@@ -4,9 +4,7 @@ using UnityEngine;
 
 public class enemySlime : MonoBehaviour
 {
-
     Animator animate;
-
     public float Health = 10;
 
     private void Start() {
@@ -21,8 +19,19 @@ public class enemySlime : MonoBehaviour
         Health -= value;
         if (Health <= 0) {
             Defeated();
+        } else {
+            takingDamage();
         }
         print(getHealth());
+    }
+
+    public void takingDamage() {
+        animate.SetBool("isTakingDamage", true);
+        animate.SetTrigger("isDamaged");
+    }
+
+    public void noDamage() {
+        animate.SetBool("isTakingDamage", false);
     }
 
     public void Defeated() {
