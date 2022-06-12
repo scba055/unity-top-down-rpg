21b11503402fc64d08e1581c4c453ad42f71bfee
Shaina Caballero <scba055@aucklanduni.ac.nz>
2022-06-12 18:49:03 +1200

    Sprite has idle and walk animations

--- TopDown2DCrashCourse/Assets/PlayerController.cs
+++ TopDown2DCrashCourse/Assets/PlayerController.cs
@@ -12,29 +12,55 @@ public class PlayerController : MonoBehaviour
 
     Vector2 movementInput;
     Rigidbody2D rb;
+    Animator animator;
     List<RaycastHit2D> castCollisions = new List<RaycastHit2D>();
 
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();   
+        rb = GetComponent<Rigidbody2D>(); 
+        animator = GetComponent<Animator>();  
     }
 
     private void FixedUpdate() 
     {
         // if movement input is not 0, try to move
         if (movementInput != Vector2.zero) {
-                int count = rb.Cast(
-                    movementInput, // direction - x and y between -1 and 1 that represent the direction from the body to look for collisions
+            bool success = TryMove(movementInput);
+
+            if (!success) {
+                success = TryMove(new Vector2(movementInput.x , 0));
+
+                if (!success) {
+                    success = TryMove(new Vector2(0, movementInput.y));
+                }
+            }
+
+            animator.SetBool("isMoving", success);
+        } else {
+            animator.SetBool("isMoving", false);
+
+        }
+
+        // set direction of sprite to movement direction
+        
+    }
+
+    private bool TryMove(Vector2 direction) {
+        // checks for potential collisions
+        int count = rb.Cast(
+                    direction, // direction - x and y between -1 and 1 that represent the direction from the body to look for collisions
                     movementFilter, // the setting that determine where a collision can occur on such as layers to collide with
                     castCollisions, // list of collisions to store the found collisions into after the cast is finished
                     moveSpeed * Time.fixedDeltaTime + collisionOffset // the amount to cast equal to the movement plus an offset
                 );
 
                 if (count == 0) {
-                    rb.MovePosition(rb.position + movementInput * moveSpeed * Time.fixedDeltaTime);
+                    rb.MovePosition(rb.position + direction * moveSpeed * Time.fixedDeltaTime);
+                    return true;
+                } else {
+                    return false;
                 }
-        }
     }
 
     void OnMove(InputValue movementValue) 
